21ba29d7e00cb50ec90e6554490ce749c016cc83
Vinicius <vpsanches00@gmail.com>
2024-11-22 21:56:41 -0300

    iniciando com JS

---
+++ main.js
@@ -0,0 +1,89 @@
+//ex1
+alert('Hello World')
+
+
+
+//ex2
+let n1 = Number(prompt('digite um numero'))
+let n2 = Number(prompt('digite outro numero'))
+
+let r = n1 + n2
+
+alert(`o resultado foi ${r}`)
+
+
+//ex3
+const p = 2
+
+if (typeof p == "number") {
+    console.log('é um número')
+} else {
+    console.log('n é um número')
+}
+
+
+//ex4
+const isstring = "stringteste"
+
+if(typeof isstring == 'string') {
+    console.log('é string')
+} else {
+    console.log('n é string')
+}
+
+
+//ex5
+const b = true
+
+if(typeof b == "boolean") {
+    console.log('é boolean')
+} else {
+    console.log('n é boolean')
+}
+
+
+//ex6
+const number1 = 5
+const number2 = 2
+
+const resul = number1 - number2
+
+console.log(`a Subtração dos valores é ${resul}`)
+
+
+//ex7
+const numb1 = 5
+const numb2 = 2
+
+const result = 5 * 2
+
+console.log(`O resultado da multiplicação é ${result}`)
+
+
+//ex8
+const numero1 = 10
+const numero2 = 2
+
+const resp = 10 / 2
+
+console.log(`A resposta da divisão entre esses valores é ${resp}`)
+
+
+//ex9
+const valor = 2
+
+if(valor % 2 == 0) {
+    console.log('é um número par')
+} else {
+    console.log('n é um número par')
+}
+
+
+//ex10
+const v = 1
+
+if(v % 2 == 1) {
+    console.log('é um número ímpar')
+} else {
+    console.log('n é um número ímpar')
+}
